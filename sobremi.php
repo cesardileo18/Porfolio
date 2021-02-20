d8303dbcf9044c84a2486379df6ca5b34598c3ce
cesardileo18 <cesardileo18@gmail.com>
2021-02-20 12:19:30 -0300

    c

--- sobremi.php
+++ sobremi.php
@@ -26,7 +26,7 @@
             <li><a href="index.php"><span class="icon-home"></span>Inicio</a></li>
             <li><a href="sobremi.php"><span class="icon-user-tie"></span>Sobre mi</a></li>
             <li><a href="proyectos.php"><span class="icon-briefcase"></span>Proyectos</a></li>
-            <li><a href="contacto.php"><span class="icon-envelop"></span>Contactos</a></li>
+            <li><a href="contacto.php"><span class="icon-envelop"></span>Contacto</a></li>
         </ul>
     </nav>
 </header>
